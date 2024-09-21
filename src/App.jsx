bfa4a7ae2bd95dac11f07eb5c2064f98206319a5
Deepak Singh <deepaksingh.b@gmail.com>
2024-09-21 19:33:45 +0530

    Finished the landing page html and CSS in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="container">
+        <div class="half left">
+          <section>
+            <div className="left-section">
+              <h1>Welcome to Our Conference</h1>
+              <h3>Plan your major event with us</h3>
+              <button class="started">Get Started</button>
+            </div>
+          </section>
+        </div>
+
+        <div class="half right">
+          <section>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam.
+              </p>
+              <p>
+                Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis
+                sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue
+                semper porta.
+              </p>
+              <p>
+                Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent
+                taciti sociosqu ad litora torquent per conubia nostra, per
+                inceptos himenaeos.
+              </p>
+            </div>
+          </section>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
